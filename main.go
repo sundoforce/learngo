195fc2c6882c8751d4021a8e5e4a97ea598046e7
sdk <admin@sundo.kim>
2020-05-02 08:30:20 +0900

    learn for loop

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func lenAndUpper(name string) (length int, uppercase string) {
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+func superAdd(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
 }
+
 func main() {
-	totallength, up := lenAndUpper("sdk")
-	fmt.Println(totallength, up)
+	result := superAdd(1, 2, 3, 4, 5)
+	fmt.Println(result)
 }
