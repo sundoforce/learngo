672a37cef6bb609077091491bd8b8b20a8ade8b8
sdk <admin@sundo.kim>
2020-05-11 06:27:15 +0900

    wsitcth naked return

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import "fmt"
 
 func canIDrink(age int) bool {
-	switch {
-	case age < 18:
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
 		return false
-	case age == 18:
+	case 18:
 		return true
-	case age > 50:
-		return false
 	}
 	return false
 }
