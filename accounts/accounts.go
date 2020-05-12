b7b7398d184f596db85d593d5f36676046c7a040
sdk <admin@sundo.kim>
2020-05-13 08:57:27 +0900

    baking

---
+++ accounts/accounts.go
@@ -0,0 +1,23 @@
+package accounts
+
+// Account strurct
+type Account struct {
+	owner   string
+	balance int
+}
+
+// NewAccount creates Account
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
+
+// Deposit x amount on your account
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Balance of your
+func (a Account) Balance() int {
+	return a.balance
+}
--- main.go
+++ main.go
@@ -1,16 +1,7 @@
 package main
 
-import "fmt"
-
-type pserson struct {
-	name    string
-	age     int
-	favFood []string
-}
-
 func main() {
-	favFood := []string{"kimchi", "ramen"}
-	nico := pserson{name: "nico", age: 18, favFood: favFood}
-	fmt.Println(nico)
+	account := accounts.NewAcount("nico")
+	account.Deposit(10)
 
 }
