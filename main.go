5114b112f7d162ed190696991761bf82f6d937e8
sdk <admin@sundo.kim>
2020-10-10 16:03:08 +0900

    page

--- main.go
+++ main.go
@@ -9,6 +9,16 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+
 func main() {
 	totalPages := getPages()
 
@@ -17,12 +27,29 @@ func main() {
 	}
 }
 
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
-
 func getPage(page int) {
 	pageURL := baseURL + "%start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("data-jk")
+		title := card.Find(".title>a").Text()
+		location := card.Find(".sjcl").Text()
+		fmt.Println(id, title, location)
+	})
+
 }
+
 func getPages() int {
 	pages := 0
 	res, err := http.Get(baseURL)
@@ -36,6 +63,7 @@ func getPages() int {
 	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
 		fmt.Println(s.Find("a").Length())
 	})
+
 	return 0
 
 }
